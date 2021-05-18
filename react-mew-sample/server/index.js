e0931bfd2ff990f899b1fddbe1e73de2477fddcd
Raya Straus <raya@cloudinary.com>
2021-05-18 16:16:06 +0300

    update gitignore

--- react-mew-sample/server/index.js
+++ react-mew-sample/server/index.js
@@ -7,7 +7,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.json({ message: "Hey! This is your server response!" });
+  res.send('Hello World!');
 });
 
 app.listen(3001, () =>
