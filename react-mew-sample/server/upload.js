1ba92113244c26e287a7b8f84d546f73b9992627
Raya Straus <raya@cloudinary.com>
2021-05-23 19:19:56 +0300

    Added upload to app

---
+++ react-mew-sample/server/upload.js
@@ -0,0 +1,25 @@
+require('dotenv').load();
+const cloudinary = require("cloudinary").v2;
+
+/**
+ * Upload assets by running npm run upload or npm start
+ */
+if (typeof (process.env.CLOUDINARY_URL) === 'undefined') {
+  console.warn('!! cloudinary config is undefined !!');
+  console.warn('export CLOUDINARY_URL or set env file');
+} else {
+  // Upload assets
+  cloudinary.uploader.upload('./images/tshirt.png', {public_id: 'tshirt'})
+    .then((result) => {
+      console.log(result)
+    }).catch((error) => {
+    console.log('error', error);
+  });
+
+  cloudinary.uploader.upload('./images/notebook.png', {public_id: 'notebook'})
+    .then((result) => {
+      console.log(result)
+    }).catch((error) => {
+    console.log('error', error);
+  });
+}
--- react-mew-sample/src/App.js
+++ react-mew-sample/src/App.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div></div>
+    <div>hello</div>
   );
 }
 
