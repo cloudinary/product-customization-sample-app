b39668bae6f8db70ef40ccccd16a234966b6c3d8
Raya Straus <raya@cloudinary.com>
2021-05-31 13:10:15 +0300

    updated to use functional component

--- react-mew-sample/src/App.js
+++ react-mew-sample/src/App.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import MediaEditor from "./MediaEditor";
+import editorConfig from './editorConfig';
+
+function launchEditor(myEditor){
+  myEditor.update(editorConfig);
+  myEditor.show();
+  myEditor.on("export",function(data){
+    console.log(data);
+  });
+}
 
 function App() {
+  const [mediaEditor, setMediaEditor] = useState({});
+
+  useEffect(() => {
+    const myEditor = window.cloudinary.mediaEditor({ appendTo: document.getElementById('widget') });
+    setMediaEditor(myEditor);
+    launchEditor(myEditor)
+  },[]);
   return (
     <div>
       <div style={{height: '100px'}}/>
       <hr/>
-      <MediaEditor/>
+      <div className={'widget'} id="widget" />
     </div>
   );
 }
---
+++ react-mew-sample/src/editorConfig.js
@@ -0,0 +1,74 @@
+let editorConfig = {
+  publicIds: [{publicId: 'tshirt'}],
+  // update cloudName
+  cloudName: "rcstraus",
+  mode: 'inline',
+  image: {
+    steps: ["imageOverlay", "textOverlays", "export"],
+    textOverlays: {
+      fonts: [
+        "Arial",
+        "Times New Roman"
+      ],
+      presets: [
+        {
+          "label": "select to add text",
+          "fontSize": 40,
+        }
+      ]
+    },
+    imageOverlay: {
+      overlays: [
+        {
+          "publicId": "logo",
+          "label": "Logo",
+          "transformation": [{ "effect": "blackwhite" }],
+          "placementOptions": [
+            {
+              "x": 200,
+              "y": 283,
+              "width": 40,
+              "height": 40,
+              "gravity": "north_west"
+            },
+            {
+              "x": 200,
+              "y": 283,
+              "width": 40,
+              "height": 40,
+              "gravity": "north_east"
+            },
+            {
+              "x": 200,
+              "y": 283,
+              "width": 40,
+              "height": 40,
+              "gravity": "south_east"
+            },
+            {
+              "x": 200,
+              "y": 283,
+              "width": 40,
+              "height": 40,
+              "gravity": "south_west"
+            },
+            {
+              "x": 0,
+              "y": 0,
+              "width": 200,
+              "height": 40,
+              "gravity": "center"
+            }
+          ]
+        },
+      ]
+    },
+    "quality": ["auto"],
+    "download": true,
+    "share": true,
+
+
+  }
+};
+
+export default editorConfig;
