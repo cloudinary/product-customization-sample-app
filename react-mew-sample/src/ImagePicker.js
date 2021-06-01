dcfd04098922d671c0465d1b2fca5884423aa4c3
Raya Straus <raya@cloudinary.com>
2021-06-01 10:00:01 +0300

    update transformation on picker

--- react-mew-sample/src/ImagePicker.js
+++ react-mew-sample/src/ImagePicker.js
@@ -6,12 +6,12 @@ function ImagePicker(props){
     <div className={"row"}>
       <div className={"column"} >
         <img
-             src="http://res.cloudinary.com/rcstraus/image/upload/w_50,c_scale/notebook"
+             src="http://res.cloudinary.com/rcstraus/image/upload/w_55,h_55,c_scale/notebook"
              onClick={()=> {props.mediaEditor.update({publicIds: [{publicId:'notebook'}]})}}/>
       </div>
       <div className={"column"} >
         <img
-             src="http://res.cloudinary.com/rcstraus/image/upload/w_50,c_scale/tshirt"
+             src="http://res.cloudinary.com/rcstraus/image/upload/w_55,h_55,c_scale/tshirt"
              onClick={()=> {props.mediaEditor.update({publicIds: [{publicId:'tshirt'}]})}}/>
       </div>
     </div>
